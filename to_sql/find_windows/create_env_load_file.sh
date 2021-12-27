f7c568bbc61015dbcbce664af61232eab9efd98b
Amin AzGol <golestani76@yahoo.com>
2021-12-27 03:48:06 +0330

    chmod

--- to_sql/find_windows/create_env_load_file.sh
+++ to_sql/find_windows/create_env_load_file.sh
@@ -1,5 +1,6 @@
 # load environment variables
 printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export PG" > /src/project_env.sh
 echo "echo ENV variables are loaded" >> /src/project_env.sh
+chmod 777 /src/project_env.sh
 echo "ENV load file created"
 
